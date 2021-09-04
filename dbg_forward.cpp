8aeeb5c26f8866fd23ca742bd8aa607dd23dc349
Jonathan Barrow <jonbarrow1998@gmail.com>
2021-09-04 08:02:25 -0400

    very old source. please dont use. needs fixing

--- dbg_forward.cpp
+++ dbg_forward.cpp
@@ -200,6 +200,6 @@
 
 
 // minidumpapiset.h
-// export these to dbgcore so that the final built DLL can be named either dbghelp or dbgcore, for use with CemuHook
+// export these to dbgcore so that the final built DLL can be named either dbghelp or dbgcore
 #pragma comment(linker, "/export:MiniDumpReadDumpStream=C:\\Windows\\System32\\dbgcore.MiniDumpReadDumpStream")
 #pragma comment(linker, "/export:MiniDumpWriteDump=C:\\Windows\\System32\\dbgcore.MiniDumpWriteDump")
